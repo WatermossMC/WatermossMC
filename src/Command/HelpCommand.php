ebe2299564fd2c79f954486016ece25087d387cd
NurAzli <144025939+NurAzli@users.noreply.github.com>
2025-01-24 06:01:12 +0700

    Create HelpCommand.php

---
+++ src/Command/HelpCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WatermossMC\Command;
+
+use WatermossMC\Server;
+
+class HelpCommand extends Command
+{
+    private $server;
+
+    public function __construct(Server $server)
+    {
+        parent::__construct("help", "Displays the list of available commands.");
+        $this->server = $server;
+    }
+
+    public function execute(array $args): void
+    {
+        echo "Available commands:\n";
+        foreach ($this->server->getCommandManager()->getCommands() as $command) {
+            echo "- " . $command->getName() . ": " . $command->getDescription() . "\n";
+        }
+    }
+}
