127d61348b246e305be64bd04efff33a3cdf518f
GroXzaLYs <144025939+GroXzaLYs@users.noreply.github.com>
2025-11-04 19:29:05 +0700

    Refactor CommandManager to use typed array for commands

--- src/Command/CommandManager.php
+++ src/Command/CommandManager.php
@@ -3,11 +3,25 @@
 namespace WatermossMC\Command;
 
 use WatermossMC\Server;
+// Pastikan kelas-kelas command yang digunakan di-import jika berada di namespace lain
+// use WatermossMC\Command\StopCommand; 
+// use WatermossMC\Command\HelpCommand;
 
+/**
+ * @property array<string, Command> $commands
+ * Tipe array spesifik dideklarasikan melalui PHPDoc agar PHPStan tahu bahwa
+ * array ini berisi objek Command yang di-indeks oleh string (nama command).
+ */
 class CommandManager
 {
     private Server $server;
-    private $commands = [];
+
+    /**
+     * @var Command[]|array<string, Command>
+     * Kita menggunakan sintaks array dasar 'array' untuk PHP, dan PHPDoc
+     * untuk memberikan detail lebih lanjut kepada PHPStan.
+     */
+    private array $commands = []; // Menambahkan 'array' sebagai tipe (PHP 7.4+)
 
     public function __construct(Server $server)
     {
@@ -17,24 +31,30 @@ class CommandManager
 
     public function registerCommand(Command $command): void
     {
+        // Karena CommandManager tahu bahwa $command adalah instance dari Command,
+        // dan getName() mengembalikan string, penambahan ke array ini aman.
         $this->commands[$command->getName()] = $command;
     }
 
     public function handleCommand(string $input): bool
     {
         $parts = explode(" ", $input);
+        /** @var string $commandName */
         $commandName = array_shift($parts);
         $args = $parts;
 
         if (isset($this->commands[$commandName])) {
-            $this->commands[$commandName]->execute($args);
+            /** @var Command $command */
+            $command = $this->commands[$commandName];
+
+            $command->execute($args); 
             return true;
         }
 
         return false;
     }
 
-    public function getServer(): void
+    public function getServer(): Server
     {
         return $this->server;
     }
