5ee0fda6f35669270f4c5d0eafc2e8ba46b7cd94
GroXzaLYs <144025939+GroXzaLYs@users.noreply.github.com>
2025-11-04 19:30:00 +0700

    Clean up CommandManager.php by removing comments
    Removed commented-out code and PHPDoc comments for clarity.

--- src/Command/CommandManager.php
+++ src/Command/CommandManager.php
@@ -3,25 +3,15 @@
 namespace WatermossMC\Command;
 
 use WatermossMC\Server;
-// Pastikan kelas-kelas command yang digunakan di-import jika berada di namespace lain
-// use WatermossMC\Command\StopCommand; 
-// use WatermossMC\Command\HelpCommand;
 
-/**
- * @property array<string, Command> $commands
- * Tipe array spesifik dideklarasikan melalui PHPDoc agar PHPStan tahu bahwa
- * array ini berisi objek Command yang di-indeks oleh string (nama command).
- */
 class CommandManager
 {
     private Server $server;
 
     /**
      * @var Command[]|array<string, Command>
-     * Kita menggunakan sintaks array dasar 'array' untuk PHP, dan PHPDoc
-     * untuk memberikan detail lebih lanjut kepada PHPStan.
      */
-    private array $commands = []; // Menambahkan 'array' sebagai tipe (PHP 7.4+)
+    private array $commands = [];
 
     public function __construct(Server $server)
     {
@@ -31,8 +21,6 @@ class CommandManager
 
     public function registerCommand(Command $command): void
     {
-        // Karena CommandManager tahu bahwa $command adalah instance dari Command,
-        // dan getName() mengembalikan string, penambahan ke array ini aman.
         $this->commands[$command->getName()] = $command;
     }
 
