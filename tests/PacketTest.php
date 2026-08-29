33aba3a7f0e63e04dca5764868e3a7584c9fad14
AzliRynz <nurazligaming@gmail.com>
2026-08-29 21:33:59 +0700

    cs fix

--- tests/PacketTest.php
+++ tests/PacketTest.php
@@ -1,14 +1,29 @@
 <?php
 
+/*
+ * __        __    _                                    __  __  ____
+ * \ \      / /_ _| |_ ___ _ __ _ __ ___   ___  ___ ___|  \/  |/ ___|
+ *  \ \ /\ / / _` | __/ _ \ '__| '_ ` _ \ / _ \/ __/ __| |\/| | |
+ *   \ V  V / (_| | ||  __/ |  | | | | | | (_) \__ \__ \ |  | | |___
+ *    \_/\_/ \__,_|\__\___|_|  |_| |_| |_|\___/|___/___/_|  |_|\____|
+ *
+ * WatermossMC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author WatermossMC Team
+ * @link https://github.com/watermossmc/WatermossMC
+ */
+
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use watermossmc\network\mcpe\protocol\ProtocolInfo;
 use watermossmc\network\mcpe\protocol\InboundPacketRegistry;
-use watermossmc\network\mcpe\protocol\serverbound\MovePlayer;
-use watermossmc\network\mcpe\protocol\serverbound\PlayerAuthInput;
-use watermossmc\network\mcpe\protocol\clientbound\Text;
-use watermossmc\binary\Binary;
+use watermossmc\network\mcpe\protocol\ProtocolInfo;
+use watermossmc\network\Session;
 
 class PacketTest extends TestCase
 {
@@ -22,7 +37,7 @@ class PacketTest extends TestCase
     {
         $registry = new InboundPacketRegistry();
         $dispatched = false;
-        $registry->register(0xfe, function(string $packet, int &$offset, \watermossmc\network\Session $session, \Socket $socket) use (&$dispatched) {
+        $registry->register(0xfe, function (string $packet, int &$offset, Session $session, \Socket $socket) use (&$dispatched): void {
             $dispatched = true;
         });
 
